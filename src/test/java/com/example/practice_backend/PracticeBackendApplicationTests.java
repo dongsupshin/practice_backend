2821ff378aac612a8d15d53793091aa87f70b81f
dongsupshin <dongsupshin.ds@gmail.com>
2025-12-30 00:29:42 +0900

    first upload gradle java spring project for practice backend

---
+++ src/test/java/com/example/practice_backend/PracticeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
